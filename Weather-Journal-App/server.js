20ea2847f1a8192f0c0ca67ccde49f81f3b9d7b1
Maysara Safwat <maisarasafwat2@gmail.com>
2021-11-10 13:49:44 +0200

    initial setup & add starter code

---
+++ Weather-Journal-App/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use(cors());
+ 
+const port = 8080
+app.listen(port, ()=>{
+    console.log(`listening on port : ${port}`)
+});
