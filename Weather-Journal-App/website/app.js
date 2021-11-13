464e9459f79befcbfb14e7a931f30bb205b3189f
Maysara Safwat <maisarasafwat2@gmail.com>
2021-11-13 13:07:59 +0200

    corrected date variable

--- Weather-Journal-App/website/app.js
+++ Weather-Journal-App/website/app.js
@@ -15,7 +15,7 @@ const entryEl = document.querySelector(".entry");
 const errorEl = document.querySelector(".error");
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 //getting the weather data obj
 const getData = async (zipCode)=>{
